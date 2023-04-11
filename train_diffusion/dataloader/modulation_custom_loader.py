97dcfc67052d9d3ead83c9e80b1691c172adb0d8
Muhammad Zubair Irshad <muhammadzubairirshad@gmail.com>
2023-04-11 14:38:03 -0400

    custom sdf dataloader for stage 1 training

--- train_diffusion/dataloader/modulation_custom_loader.py
+++ train_diffusion/dataloader/modulation_custom_loader.py
@@ -5,6 +5,7 @@ import torch.utils.data
 
 import pandas as pd 
 import numpy as np
+import random
 
 class ModulationLoaderCustom(torch.utils.data.Dataset):
     def __init__(self, data_path, pc_path=None, split_file=None, pc_size=None):
@@ -16,6 +17,7 @@ class ModulationLoaderCustom(torch.utils.data.Dataset):
 
         self.data_source = data_path
         self.pc_source = pc_path
+        self.samples_per_epoch = 100*self.folders
         
     def get_pcd(self, f, pc_size =1024):
         f=pd.read_csv(f, sep=',',header=None).values
@@ -26,20 +28,22 @@ class ModulationLoaderCustom(torch.utils.data.Dataset):
         return pc
     
     def __len__(self):
-        return len(self.folders)
+        # return len(self.folders)
+        return self.samples_per_epoch
 
     def __getitem__(self, index):
         
-        modulation_filename = os.path.join(self.data_source, self.folders[index], 'latent.pth')
+        train_idx = random.randint(0, len(self.folders) - 1)
+        modulation_filename = os.path.join(self.data_source, self.folders[train_idx], 'latent.pth')
         # latent = torch.from_numpy(np.loadtxt(modulation_filename)).float()
-        latent = torch.load(modulation_filename).float()
+        latent = torch.load(modulation_filename, map_location=torch.device('cpu')).float()
 
-        # if self.conditional:
-        #     pcd_file_name = os.path.join(self.pc_source, self.folders[index], 'pcd.csv')
-        #     pc = self.get_pcd(pcd_file_name, pc_size=self.pc_size)
-        # else:
-        #     pc = False
+        if self.conditional:
+            pcd_file_name = os.path.join(self.pc_source, self.folders[train_idx], 'pcd.csv')
+            pc = self.get_pcd(pcd_file_name, pc_size=self.pc_size)
+        else:
+            pc = False
 
-        pc = False
+        # pc
 
         return latent, pc
