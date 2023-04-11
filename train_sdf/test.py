b225f4e03882589ca4e8dfc23e346fed73476056
Muhammad Zubair Irshad <muhammadzubairirshad@gmail.com>
2023-04-10 23:40:34 -0400

    custom sdf dataloader for stage 1 training

--- train_sdf/test.py
+++ train_sdf/test.py
@@ -33,7 +33,7 @@ def test_modulations():
     # load dataset, dataloader, model checkpoint
     # test_split = json.load(open(specs["TestSplit"]))
     # test_dataset = PCloader(specs["DataSource"], test_split, pc_size=specs.get("PCsize",1024), return_filename=True)
-    test_dataset = PCloader(specs["DataSource"], pc_size=specs.get("PCsize",1024), return_filename=True)
+    test_dataset = PCloader(specs["DataSource"], pc_size=specs.get("PCsize",1024))
     test_dataloader = torch.utils.data.DataLoader(test_dataset, batch_size=1, num_workers=0)
 
     ckpt = "{}.ckpt".format(args.resume) if args.resume=='last' else "epoch={}.ckpt".format(args.resume)
